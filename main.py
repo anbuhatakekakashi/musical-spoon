cecb7855ec08836c83b665f6dd60905d6c22b92d
fdiaz9 <fdiaz9@gatech.edu>
2017-03-28 13:58:24 -0400

    Adding to main.py

--- main.py
+++ main.py
@@ -1,21 +1,69 @@
+'''
+SCOREBOARD DESIGN IDEA
+
+    TEAM 1 - 90 pts
+
+SGT Andrews:    20 pts
+SPC Bradley:    30 pts
+SGT Christian:  40 pts
+
+    TEAM 2 - 110 pts
+
+SGT Davis:      50 pts
+SSG Eckland:    50 pts
+SSG Forehand:   10 pts
+
+'''
+
+#  This is a dictionary consisting of question numbers as keys, and their associated hashes as values
+
+known_hashes = {1: "AAAAAAAAAAAAAAAAAAAA",
+                2: "BBBBBBBBBBBBBBBBBBBB",
+                3: "CCCCCCCCCCCCCCCCCCCC"}
+
+class Scoreboard:
+    pass
 
 class Player:
     name = ""
     rank = ""
-
     def add_player(self, playerName, playerRank):
         Player.name = playerName
         Player.rank = playerRank
 
+def hash_checker(question, hash):
+
+    # Check if hash is between 20 and 30 characters
+
+    if (len(hash) < 20):
+        raise Exception("String is too small")
+    elif (len(hash) > 30):
+        raise Exception("String is too large")
+
+    if (known_hashes[question] == hash):
+        print("CORRECT!")
+        # send success message back to user
+        # update scoreboard
+    else:
+        print("WRONG!")
+        # notify user they got the message wrong
+
+
+
 def display_menu():
-    print("1.  Display teams")
-    print("2.  Display scores")
+    print("\n")
+    print("1.  Add/remove player")
+    print("2.  Display teams")
+    print("3.  Display scores")
     print("99. Exit")
+
+def display_scoreboard():
+    pass
 
 def main():
     valid = True
     while (valid):
-        print("\n")
+        # display_scoreboard()
         display_menu()
         choice = input("Enter option: ")
         if(choice == "1"):
